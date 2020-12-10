1b73dc77fff5d90bbca3e222cc474905ca7d7944
amar <amarbugarin@outlook.com>
2020-12-10 02:00:16 +0100

    refactoring stuff

--- public/js/app.js
+++ public/js/app.js
@@ -1,56 +1,25 @@
-// console.log('Swamba');
-
-// fetch("http://localhost:3000/weather/?address=chester").then((response)=>{
-
-//     response.json().then((data)=>{
-//      if(data.error){
-//          console.log(data.error);
-         
-//      }else{
-//      console.log(data.location);
-//      console.log(data.weather);
-     
-     
-//      }  
-//     })
-
- 
 
 
-// })
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
 
+messageOne.textContent = "";
 
-const weatherForm=document.querySelector('form');
-const search=document.querySelector('input');
-const messageOne = document.querySelector('#message-1');
-const messageTwo=document.querySelector('#message-2')
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const location = search.value;
 
-messageOne.textContent="";
-
-
-weatherForm.addEventListener('submit',(e)=>{
-    e.preventDefault();
-const location=search.value    
-    
-
-    fetch("/weather/?address="+location).then((response)=>{
-
-    response.json().then((data)=>{
-     if(data.error){
-        messageOne.textContent=data.error;
-        messageTwo.textContent=""
-         
-     }else{
-        messageOne.textContent=data.location;
-        messageTwo.textContent=data.weather;
-    
-     
-     }  
-    })
-
- 
-
-
-})
-    
+  fetch("/weather/?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+        messageTwo.textContent = "";
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.weather;
+      }
+    });
+  });
 });
